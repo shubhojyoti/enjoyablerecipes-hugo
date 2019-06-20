f543b4fbc8cfa89b53eee3f99c09d7989f89a0e5
Shubhojyoti Bhattacharya <shubhojyoti.b@gmail.com>
2019-06-20 05:53:34 +0530

    Updated to latest node and hugo versions

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,17 @@
-let     gulp = require('gulp'),
-         del = require('del'),
- runSequence = require('run-sequence'),
-          es = require('event-stream'),
-      rename = require('gulp-rename'),
-        sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps'),
-  browserify = require('browserify'),
-    babelify = require('babelify'),
-      source = require('vinyl-source-stream'),
-      buffer = require('vinyl-buffer'),
-      uglify = require('gulp-uglify'),
-      minify = require('gulp-minify-css');
+'use strict';
+
+let gulp = require('gulp'),
+    del = require('del'),
+    merge = require('merge-stream'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    minify = require('gulp-minify-css');
 
 let config = {
     'main_scss': './src-assets/scss/style.scss',
@@ -30,45 +30,49 @@ let config = {
     }
 };
 
-gulp.copy=function(src,dest, base){
-    if (base === undefined) { base = '.'; }
+gulp.copy = function (src, dest, base) {
+    if (base === undefined) {
+        base = '.';
+    }
     return gulp.src(src, {base: base})
         .pipe(gulp.dest(dest));
 };
 
-gulp.task('clean:dev', function () {
+gulp.task('clean:dev', () => {
     return del(['./dev']);
 });
 
-gulp.task('clean:public', function () {
+gulp.task('clean:public', () => {
     return del(['./public']);
 });
 
 // Clears the dynamically generated directories inside static
-gulp.task('clean:static:all', function () {
+gulp.task('clean:static:all', () => {
     return del([config.static.css, config.static.js, config.static.fonts,
-                config.static.images]);
+        config.static.images]);
 });
 
 // Clears static/js
-gulp.task('clean:static:js', function () {
+gulp.task('clean:static:js', () => {
     return del([config.static.js + '/**/*']);
 });
 // Clears static/css
-gulp.task('clean:static:css', function () {
+gulp.task('clean:static:css', () => {
     return del([config.static.css + '/**/*']);
 });
-gulp.task('static:css', function() {
-    gulp.src(config.source.css + '/**/*')
-            .pipe(gulp.dest(config.static.css));
-})
+gulp.task('static:css', () => {
+    return gulp.src(config.source.css + '/**/*')
+        .pipe(gulp.dest(config.static.css));
+});
 
 // Compiles Sass to CSS and copies to static/css
-gulp.task('build:css:dev', function() {
+gulp.task('build:css:dev', () => {
     return gulp.src(config.main_scss)
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .on('error', function(error) { console.log(error); })
+        .on('error', function (error) {
+            console.log(error);
+        })
         .pipe(sourcemaps.write({includeContent: false}))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(autoprefixer())
@@ -77,10 +81,12 @@ gulp.task('build:css:dev', function() {
         .pipe(gulp.dest(config.static.css));
 });
 
-gulp.task('build:css:prod', function() {
+gulp.task('build:css:prod', () => {
     return gulp.src(config.main_scss)
         .pipe(sass())
-        .on('error', function(error) { console.log(error); })
+        .on('error', function (error) {
+            console.log(error);
+        })
         .pipe(autoprefixer())
         .pipe(minify())
         .pipe(rename('style.css'))
@@ -88,10 +94,10 @@ gulp.task('build:css:prod', function() {
 });
 
 // Builds JS
-gulp.task('build:js:dev', function () {
-    return es.concat(
+gulp.task('build:js:dev', () => {
+    return merge(
         browserify({entries: config.source.js + '/recipelist.js', debug: true})
-            .transform("babelify", { presets: ["es2015"] })
+            .transform('babelify', { presets: ['@babel/preset-env'] })
             .bundle()
             .pipe(source('recipelist.js'))
             .pipe(buffer())
@@ -99,7 +105,7 @@ gulp.task('build:js:dev', function () {
             .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest(config.static.js)),
         browserify({entries: config.source.js + '/homepage.js', debug: true})
-            .transform("babelify", { presets: ["es2015"] })
+            .transform('babelify', { presets: ['@babel/preset-env'] })
             .bundle()
             .pipe(source('homepage.js'))
             .pipe(buffer())
@@ -107,7 +113,7 @@ gulp.task('build:js:dev', function () {
             .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest(config.static.js)),
         browserify({entries: config.source.js + '/scripts.js', debug: true})
-            .transform("babelify", { presets: ["es2015"] })
+            .transform('babelify', { presets: ['@babel/preset-env'] })
             .bundle()
             .pipe(source('scripts.js'))
             .pipe(buffer())
@@ -117,24 +123,24 @@ gulp.task('build:js:dev', function () {
     );
 });
 
-gulp.task('build:js:prod', function () {
-    return es.concat(
+gulp.task('build:js:prod', () => {
+    return merge(
         browserify({entries: config.source.js + '/recipelist.js', debug: true})
-            .transform("babelify", { presets: ["es2015"] })
+            .transform('babelify', { presets: ['@babel/preset-env'] })
             .bundle()
             .pipe(source('recipelist.js'))
             .pipe(buffer())
             .pipe(uglify())
             .pipe(gulp.dest(config.static.js)),
         browserify({entries: config.source.js + '/homepage.js', debug: true})
-            .transform("babelify", { presets: ["es2015"] })
+            .transform('babelify', { presets: ['@babel/preset-env'] })
             .bundle()
             .pipe(source('homepage.js'))
             .pipe(buffer())
             .pipe(uglify())
             .pipe(gulp.dest(config.static.js)),
         browserify({entries: config.source.js + '/scripts.js', debug: true})
-            .transform("babelify", { presets: ["es2015"] })
+            .transform('babelify', { presets: ['@babel/preset-env'] })
             .bundle()
             .pipe(source('scripts.js'))
             .pipe(buffer())
@@ -144,8 +150,8 @@ gulp.task('build:js:prod', function () {
 });
 
 // Copy resources to static
-gulp.task('static', function() {
-    return es.concat(
+gulp.task('static', () => {
+    return merge(
         gulp.src(config.source.fonts + '/**/*')
             .pipe(gulp.dest(config.static.fonts)),
         gulp.src(config.source.images + '/**/*')
@@ -155,32 +161,32 @@ gulp.task('static', function() {
     );
 });
 
-gulp.task('jswatch', function() {
-    gulp.watch(config.source.js + '/**/*', function() {
-        runSequence('clean:static:js', 'build:js:dev');
-    });
+gulp.task('jswatch', () => {
+    gulp.watch(config.source.js + '/**/*', gulp.series('clean:static:js', 'build:js:dev'));
 });
 
-gulp.task('csswatch', function() {
-    gulp.watch(config.source.scss + '/**/*', function() {
-        runSequence('clean:static:css', 'static:css', 'build:css:dev');
-    });
+gulp.task('csswatch', function () {
+    gulp.watch(config.source.scss + '/**/*', gulp.series('clean:static:css', 'static:css', 'build:css:dev'));
 });
 
-gulp.task('clean', ['clean:static:all', 'clean:dev', 'clean:public']);
+gulp.task('clean', gulp.parallel('clean:static:all', 'clean:dev', 'clean:public'));
 
 // Production gulp task
-gulp.task('prod', function(callback) {
-    runSequence('clean',
-                'static',
-                ['build:css:prod', 'build:js:prod']);
-});
+gulp.task('prod',
+    gulp.series(
+        'clean',
+        'static',
+        gulp.parallel('build:css:prod', 'build:js:prod')
+    )
+);
 
 // Default gulp task for dev
-gulp.task('default', function(callback) {
-    runSequence('clean',
-                'static',
-                ['build:css:dev', 'build:js:dev']);
-});
+gulp.task('default',
+    gulp.series(
+        'clean',
+        'static',
+        gulp.parallel('build:css:dev', 'build:js:dev')
+    )
+);
 
-gulp.task('watch', ['csswatch', 'jswatch']);
+gulp.task('watch', gulp.parallel('csswatch', 'jswatch'));
